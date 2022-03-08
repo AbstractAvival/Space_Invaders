0aac6a2b1ed83078e90d9776869b8a72e7d52e66
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-07 19:44:55 -0600

    Creation and skeleton implementation of class StateHandler.

---
+++ Space_Invaders/StateHandler.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "StateTypes.h"
+using namespace std;
+
+class StateHandler
+{
+public:
+	StateHandler() = default;
+	StateHandler( sf::RenderWindow& window, int screenWidth, int screenHeight );
+	~StateHandler();
+
+	void InitializeBeginningState();
+	void ChangeState( StateTypes targetState );
+
+	GameState GetCurrentState();
+	GameState GetState( StateTypes targetState );
+
+private:
+	vector< GameState > gameStates;
+	StateTypes currentState;
+	StateTypes lastState;
+};
