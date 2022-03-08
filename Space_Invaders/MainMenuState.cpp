73d92eeaca62266c3ab01f736103147d2be23c73
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-08 13:10:38 -0600

    Creation and basic skeleton implementation of class MainMenuState.

---
+++ Space_Invaders/MainMenuState.cpp
@@ -0,0 +1,30 @@
+#include "MainMenuState.h"
+
+MainMenuState::MainMenuState( sf::RenderWindow& windowIn, StateHandler& handlerIn, int screenWidthIn, int screenHeightIn )
+	:
+	GameState( windowIn, handlerIn, screenWidthIn, screenHeightIn )
+{}
+
+void MainMenuState::InitializeState()
+{
+}
+
+void MainMenuState::ResetState()
+{
+}
+
+void MainMenuState::Enter()
+{
+}
+
+void MainMenuState::HandleInput()
+{
+}
+
+void MainMenuState::UpdateLogic()
+{
+}
+
+void MainMenuState::Render( float interpolation )
+{
+}
---
+++ Space_Invaders/MainMenuState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameState.h"
+#include "StateHandler.h"
+
+class MainMenuState : public GameState
+{
+public:
+	MainMenuState( sf::RenderWindow& windowIn, StateHandler& handlerIn, int screenWidthIn, int screenHeightIn );
+
+public:
+	void InitializeState() override;
+	void ResetState() override;
+	void Enter() override;
+
+	void HandleInput() override;
+	void UpdateLogic() override;
+	void Render( float interpolation ) override;
+
+private:
+};
