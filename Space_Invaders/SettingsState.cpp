e6aeacc8afe4601cdfb503c4a3bc4163b4ac8100
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-09 13:47:42 -0600

    Creation of class SettingsState. Skeleton implementation of SettingsState methods.

---
+++ Space_Invaders/SettingsState.cpp
@@ -0,0 +1,30 @@
+#include "SettingsState.h"
+
+SettingsState::SettingsState( sf::RenderWindow& windowIn, StateHandler& handlerIn, int screenWidthIn, int screenHeightIn )
+	:
+	GameState( windowIn, handlerIn, screenWidthIn, screenHeightIn )
+{}
+
+void SettingsState::InitializeState()
+{
+}
+
+void SettingsState::ResetState()
+{
+}
+
+void SettingsState::Enter()
+{
+}
+
+void SettingsState::HandleInput()
+{
+}
+
+void SettingsState::UpdateLogic()
+{
+}
+
+void SettingsState::Render( float interpolation )
+{
+}
---
+++ Space_Invaders/SettingsState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameState.h"
+#include "StateHandler.h"
+
+class SettingsState : public GameState
+{
+public:
+	SettingsState( sf::RenderWindow& windowIn, StateHandler& handlerIn, int screenWidthIn, int screenHeightIn );
+
+public:
+	void InitializeState() override;
+	void ResetState() override;
+	void Enter() override;
+
+	void HandleInput() override;
+	void UpdateLogic() override;
+	void Render(float interpolation) override;
+
+private:
+};
