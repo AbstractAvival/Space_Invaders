75a5cdd5a66c61bf71255cbce584df452483258c
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-09 13:49:49 -0600

    Creation of class PlayState. Skeleton implementation of PlayState methods.

---
+++ Space_Invaders/PlayState.cpp
@@ -0,0 +1,30 @@
+#include "PlayState.h"
+
+PlayState::PlayState( sf::RenderWindow& windowIn, StateHandler& handlerIn, int screenWidthIn, int screenHeightIn )
+	:
+	GameState( windowIn, handlerIn, screenWidthIn, screenHeightIn )
+{}
+
+void PlayState::InitializeState()
+{
+}
+
+void PlayState::ResetState()
+{
+}
+
+void PlayState::Enter()
+{
+}
+
+void PlayState::HandleInput()
+{
+}
+
+void PlayState::UpdateLogic()
+{
+}
+
+void PlayState::Render( float interpolation )
+{
+}
