5d2170932283058b373149642f7de65a39252945
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-14 17:59:00 -0600

    Finished implementation of method LoadGameTextures in TextureCodex class.

--- Space_Invaders/TextureCodex.cpp
+++ Space_Invaders/TextureCodex.cpp
@@ -54,18 +54,23 @@ void TextureCodex::LoadMainMenuTextures( string fileName )
 
 void TextureCodex::LoadGametextures( string fileName )
 {
-	/*
-	string gameTextureName;
+	string textureName;
+	vector< string > gameTextureNames;
 	ifstream gameTexturesFile( fileName );
 	if( gameTexturesFile.is_open() )
 	{
-		while( getline( gameTexturesFile, gameTextureName ) )
+		while( getline( gameTexturesFile, textureName ) )
 		{
-			cout << gameTextureName << endl;
+			gameTextureNames.emplace_back( textureName );
 		}
 		gameTexturesFile.close();
 	}
-	*/
+
+	for( int textureIndex = 0; textureIndex != gameTextureNames.size(); textureIndex++ )
+	{
+		gameTextures.emplace_back( new sf::Texture() );
+		gameTextures[ textureIndex ]->loadFromFile( gameTextureNames[ textureIndex ] );
+	}
 }
 
 sf::Font& TextureCodex::GetFont()
