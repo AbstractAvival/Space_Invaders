51ccbaf8b16408f892a19992fbbf02a10b762669
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-03-29 11:59:26 -0600

    Added method that returns a random float value within specified range limits.

--- Space_Invaders/RandomNumberGenerator.cpp
+++ Space_Invaders/RandomNumberGenerator.cpp
@@ -10,3 +10,9 @@ int RandomNumberGenerator::GetRandomInt( int minLimit, int maxLimit )
 	intValues = uniform_int_distribution< int >( minLimit, maxLimit );
 	return intValues( numberGenerator );
 }
+
+float RandomNumberGenerator::GetRandomFloat( float minLimit, float maxLimit )
+{
+	floatValues = uniform_real_distribution< float >( minLimit, maxLimit );
+	return floatValues( numberGenerator );
+}
--- Space_Invaders/RandomNumberGenerator.h
+++ Space_Invaders/RandomNumberGenerator.h
@@ -7,8 +7,10 @@ class RandomNumberGenerator
 public:
 	RandomNumberGenerator();
 	int GetRandomInt( int minLimit, int maxLimit );
+	float GetRandomFloat( float minLimit, float maxLimit );
 
 private:
 	mt19937 numberGenerator;
 	uniform_int_distribution< int > intValues;
+	uniform_real_distribution< float > floatValues;
 };
