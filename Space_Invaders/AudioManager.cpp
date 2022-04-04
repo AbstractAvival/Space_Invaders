5224662dba0fe8b804549d97d99464762521a4c0
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-04-04 08:25:23 -0500

    Implemented method LoadAudio and class destructor in AudioManager class.

--- Space_Invaders/AudioManager.cpp
+++ Space_Invaders/AudioManager.cpp
@@ -1,13 +1,58 @@
 #include "AudioManager.h"
 
-AudioManager::AudioManager()
+AudioManager::AudioManager( string audioFile, string musicFile )
 {
+	LoadAudio( audioFile, musicFile );
+}
+
+AudioManager::~AudioManager()
+{
+	for( auto buffer : soundBuffers )
+	{
+		delete buffer;
+		buffer = nullptr;
+	}
+
+	for( auto sound : sounds )
+	{
+		delete sound;
+		sound = nullptr;
+	}
 }
 
 void AudioManager::PlaySound( AudioTypes desiredAudio )
 {
 }
 
-void AudioManager::LoadSounds()
+void AudioManager::PlayBackgroundMusic()
 {
+}
+
+void AudioManager::StopBackgroundMusic()
+{
+}
+
+void AudioManager::LoadAudio( string audioFileName, string musicFileName )
+{
+	string audioName;
+	vector< string > audioNames;
+	ifstream audioFile( audioFileName );
+	if( audioFile.is_open() )
+	{
+		while( getline( audioFile, audioName ) )
+		{
+			audioNames.emplace_back( audioName );
+		}
+		audioFile.close();
+	}
+
+	for( int audioIndex = 0; audioIndex < int( audioNames.size() ); audioIndex++ )
+	{
+		soundBuffers.emplace_back( new sf::SoundBuffer() );
+		soundBuffers[ audioIndex ]->loadFromFile( audioNames[ audioIndex ] );
+		sounds.emplace_back( new sf::Sound() );
+		sounds[ audioIndex ]->setBuffer( *soundBuffers[ audioIndex ] );
+	}
+
+	mainGameMusic.openFromFile( musicFileName );
 }
--- Space_Invaders/AudioManager.h
+++ Space_Invaders/AudioManager.h
@@ -1,20 +1,26 @@
 #pragma once
 #include <SFML/Audio.hpp>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "AudioTypes.h"
 using namespace std;
 
 class AudioManager
 {
 public:
-	AudioManager();
+	AudioManager( string audioFile, string musicFile );
+	~AudioManager();
 	void PlaySound( AudioTypes desiredAudio );
+	void PlayBackgroundMusic();
+	void StopBackgroundMusic();
 
 private:
-	void LoadSounds();
+	void LoadAudio( string audioFileName, string musicFileName );
 
 private:
-	vector< sf::SoundBuffer > soundBuffers;
-	vector< sf::Sound > sounds;
+	vector< sf::SoundBuffer* > soundBuffers;
+	vector< sf::Sound* > sounds;
 	sf::Music mainGameMusic;
 };
